c210edd84d1f7e4c040318d76922d27539e1cd9a
willdrr <willxdd@gmailcom>
2022-09-26 23:39:05 -0600

    fixing typo

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :keep_releases, 5
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/crendentials/production.key'
+append :linked_files, 'config/credentials/production.key'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
